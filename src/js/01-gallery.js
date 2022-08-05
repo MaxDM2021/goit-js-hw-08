9ecfc851220a83590fb1a0944d3b7b47fc43836a
Maksym Dmitriiev <maximdm22@gmail.com>
2022-08-05 18:05:05 +0300

    update 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
+import '../css/common.css';
+import '../css/01-gallery.css';
+
+
+
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const palettContainer = document.querySelector(".gallery");
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+palettContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+
+function createImageCardsMarkup(galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+        return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+      `;
+      })
+      .join("");
+  }
+  
+  // обьявление "SimpleLightbox"
+  
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = "videoplayer-current-time"
+
+
+const iframe = document.querySelector('iframe');
+
+const player = Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(currentTime) {
+localStorage.getItem(STORAGE_KEY, currentTime)
+
+}
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+
+
+
+
+// const player = new Player(iframe);
+
+// document.addEventListener("DOMContentLoaded", (evt) => {
+//     // const message = evt.target.value;
+//     // const message = player.setCurrentTime(evt);
+//     localStorage.setItem(STORAGE_KEY, evt)
+//    }); 
+
+
+
+
+// player.setCurrentTime(value),
+// localeStorage.getItem(STORAGE_KEY, player )
