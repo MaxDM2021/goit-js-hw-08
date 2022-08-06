d97cc762eb23d08d79985416b431797ea7c5dd57
Maksym Dmitriiev <maximdm22@gmail.com>
2022-08-06 15:45:07 +0300

    update-6

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,88 +2,46 @@ import throttle from 'lodash.throttle';
 import '../css/common.css';
 import '../css/03-feedback.css';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+let formObject = {};
 
-const STORAGE_KEY = 'feedback-msg';
-const STORAGE_KEY2 = 'e-mail';
+form.addEventListener('input', throttle(onInputForm, 500));
+form.addEventListener('submit', onSubmitForm);
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form  textarea'),
-  input: document.querySelector('.feedback-form  input')
+updateInputForm();
+
+function onInputForm(evt) {
+  evt.preventDefault();
+  formObject[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formObject));
+}
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  const formData = { email: email.value, message: message.value };
+
+  if (email.value === '' || message.value === '') {
+    return alert('Пожалуйста, заполните пустые строки!');
+  }
+  console.log("Отправляем форму: ", formData);
+  localStorage.removeItem(STORAGE_KEY);
+  evt.currentTarget.reset();
+}
+
+function updateInputForm() {
+ const saveForm = localStorage.getItem(STORAGE_KEY);
+ const saveFormPars = JSON.parse(saveForm);
+
+ if(saveForm) {
+  form.elements.email.value = saveFormPars.email || '';
+  form.elements.message.value = saveFormPars.message || '';
+ };
 };
-
-
-refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-refs.input.addEventListener('input', throttle(onEmailInput, 500));
-
-
-populateTextarea();
-
-/*
- * - Останавливаем поведение по умолчанию
- * - Убираем сообщение из хранилища
- * - Очищаем форму
- */
-
-
-
-function onFormSubmit(evt) {
-    evt.preventDefault();
-  
-    console.log('Отправляем форму');
-    evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-
-    console.log(formData);
-  
-  }
-
-  // сделать так чтобы сохраняло не только сообщение но и имя, и все в одном обьекте
-
-  const formData = {};
-
-refs.form.addEventListener('input', e => {
-
-  formData[e.target.name] = e.target.value;
- 
-});
-
-/*
-  * - Получаем значение поля
-  * - Сохраняем его в хранилище
-  * - Можно добавить throttle
-  */
- 
-function onTextareaInput(evt) {
-   const message = evt.target.value;
- 
-   localStorage.setItem(STORAGE_KEY, message);
- }
-
- function onEmailInput(evt) {
-    const message = evt.target.value;
-  
-    localStorage.setItem(STORAGE_KEY2, message);
-  }
-
-
- /*
- * - Получаем значение из хранилища
- * - Если там что-то было, обновляем DOM
- */
-function populateTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
-    const savedEmail = localStorage.getItem(STORAGE_KEY2);
-  
-    if (savedMessage && savedEmail) {
-      refs.textarea.value = savedMessage;
-      refs.input.value = savedEmail;
-    }
-  }
-
-
-
-
 
 
